ab8173bb3947850afbf24c8297a9177ad2692654
Meraj Hasan <meraj.hasan.1988@gmail.com>
2026-08-20 13:04:29 +0500

    Enhance comments in invert_kernel function
    Added comments to explain kernel functionality and pointer usage.

--- colorinversion/unsignedchar_restrict.cu
+++ colorinversion/unsignedchar_restrict.cu
@@ -1,13 +1,17 @@
 #include <cuda_runtime.h>
-
+//launch_bounds: “This kernel uses at most 256 threads per block, and please compile it so that resources ideally permit at least 4 such blocks to be resident simultaneously on one SM.”
 __global__ void invert_kernel(
     unsigned char* __restrict__ image,
     int pixels)
+//__restrict__ is a promise to the compiler not to do pointer aliasing.
+
+//Aliasing means two different pointers might point at the same memory
 {
     const int p = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (p < pixels) {
         unsigned char* ptr = image + (p << 2);
+//p << 2 means “shift p left by 2 bits,” which for nonnegative integers is equivalent to multiplying by 4: p * 4
 
         ptr[0] = ~ptr[0];   // R
         ptr[1] = ~ptr[1];   // G
